bb9acbaab5956dcde9e27f006da0fc9a30c99299
dyrroth-11 <ashish.patel.cse19@gmail.com>
2020-11-12 13:46:48 +0530

    my orders part completed

--- store/models/order.py
+++ store/models/order.py
@@ -11,6 +11,7 @@ class Order(models.Model):
     address = models.CharField(max_length=200,blank=True)
     phone = models.CharField(max_length=50,blank=True)
     date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
 
 
     def placeOrder(self):
@@ -18,4 +19,4 @@ class Order(models.Model):
 
     @staticmethod
     def get_orders_by_customer(customer_id):
-        return Order.objects.filter(customer =customer_id)
+        return Order.objects.filter(customer =customer_id).order_by('date')
--- store/views/orders.py
+++ store/views/orders.py
@@ -10,8 +10,6 @@ class OrderView(View):
     def get(self,request):
         customer = request.session.get('customer')
         orders = Order.get_orders_by_customer(customer)
+        orders = orders.reverse()
         return render(request,'orders.html',{'orders':orders})
-
-
-
 
